c7628f4f035825329257b79dcc48583577ef9f3d
23456-Tony-Larrea <avs.larrea@yavirac.edu.ec>
2022-06-09 21:04:22 -0500

    vistas

---
+++ POSales/ProductModule.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POSales
+{
+    public partial class ProductModule : Form
+    {
+        SqlConnection cn = new SqlConnection();
+        SqlCommand cm = new SqlCommand();
+        DBConnect dbcon = new DBConnect();
+        string stitle = "Punto de venta";
+        Product product;
+        public ProductModule(Product pd)
+        {
+            InitializeComponent();
+            cn = new SqlConnection(dbcon.myConnection());
+            product = pd;
+            LoadBrand();
+            LoadCategory();
+        }
+
+        public void LoadCategory()
+        {
+            cboCategory.Items.Clear();
+            cboCategory.DataSource = dbcon.getTable("SELECT * FROM Categorias");
+            cboCategory.DisplayMember = "categoria";
+            cboCategory.ValueMember = "id";
+        }
+
+        public void LoadBrand()
+        {
+            cboBrand.Items.Clear();
+            cboBrand.DataSource = dbcon.getTable("SELECT * FROM Marcas");
+            cboBrand.DisplayMember = "marca";
+            cboBrand.ValueMember = "id";
+        }
+
+        private void picClose_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        public void Clear()
+        {
+            txtPcode.Clear();
+            txtBarcode.Clear();
+            txtPdesc.Clear();
+            txtPrice.Clear();
+            cboBrand.SelectedIndex = 0;
+            cboCategory.SelectedIndex = 0;
+            UDReOrder.Value = 1;
+
+            txtPcode.Enabled = true;
+            txtPcode.Focus();
+            btnSave.Enabled = true;
+            btnUpdate.Enabled = false;
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("Estas seguro de guardar este producto?", "Producto Guardado", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    cm = new SqlCommand("INSERT INTO Productos(codigo, codigoBarras, pDesc, bid, cid, precio, reorder)VALUES (@codigo,@codigoBarras,@pDesc,@bid,@cid,@precio, @reorder)", cn);
+                    cm.Parameters.AddWithValue("@codigo", txtPcode.Text);
+                    cm.Parameters.AddWithValue("@codigoBarras", txtBarcode.Text);
+                    cm.Parameters.AddWithValue("@pDesc", txtPdesc.Text);
+                    cm.Parameters.AddWithValue("@bid", cboBrand.SelectedValue);
+                    cm.Parameters.AddWithValue("@cid", cboCategory.SelectedValue);
+                    cm.Parameters.AddWithValue("@precio", double.Parse(txtPrice.Text));
+                    cm.Parameters.AddWithValue("@reorder", UDReOrder.Value);
+                    cn.Open();
+                    cm.ExecuteNonQuery();
+                    cn.Close();
+                    MessageBox.Show("Producto Guardado con exito.", stitle);
+                    Clear();
+                    product.LoadProduct();
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("Estas seguro de actualizar este producto?", "Actualizar producto", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    cm = new SqlCommand("UPDATE Productos SET codigoBarras=@codigoBarras,pDesc=@pDesc,bid=@bid,cid=@cid,precio=@precio, reorder=@reorder WHERE codigo LIKE @codigo", cn);
+                    cm.Parameters.AddWithValue("@codigo", txtPcode.Text);
+                    cm.Parameters.AddWithValue("@codigoBarras", txtBarcode.Text);
+                    cm.Parameters.AddWithValue("@pDesc", txtPdesc.Text);
+                    cm.Parameters.AddWithValue("@bid", cboBrand.SelectedValue);
+                    cm.Parameters.AddWithValue("@cid", cboCategory.SelectedValue);
+                    cm.Parameters.AddWithValue("@precio", double.Parse(txtPrice.Text));
+                    cm.Parameters.AddWithValue("@reorder", UDReOrder.Value);
+                    cn.Open();
+                    cm.ExecuteNonQuery();
+                    cn.Close();
+                    MessageBox.Show("Product ingresado con exito.", stitle);
+                    Clear();
+                    this.Dispose();
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ProductModule_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Dispose();
+            }
+        }
+    }
+}
---
+++ POSales/SupplierModule.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POSales
+{
+    public partial class SupplierModule : Form
+    {
+        SqlConnection cn = new SqlConnection();
+        SqlCommand cm = new SqlCommand();
+        DBConnect dbcon = new DBConnect();
+        string stitle = "Punto de venta";
+        Supplier supplier;
+        public SupplierModule(Supplier sp)
+        {
+            InitializeComponent();
+            cn = new SqlConnection(dbcon.myConnection());
+            supplier = sp;
+        }
+
+        private void picClose_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        public void Clear()
+        {
+            txtAddress.Clear();
+            txtConPerson.Clear();
+            txtEmail.Clear();
+            txtFaxNo.Clear();
+            txtPhone.Clear();
+            txtSupplier.Clear();
+
+            btnSave.Enabled = true;
+            btnUpdate.Enabled = false;
+            txtSupplier.Focus();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("Guardar este provedor? click yes para confirmar.", "CONFIRMAR", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    cn.Open();
+                    cm = new SqlCommand("Insert into Proveedores (proveedor, direccion, contactPerson, telefono, email, fax) values (@proveedor, @direccion, @contactPerson, @telefono, @email, @fax) ", cn);
+                    cm.Parameters.AddWithValue("@proveedor", txtSupplier.Text);
+                    cm.Parameters.AddWithValue("@direccion", txtAddress.Text);
+                    cm.Parameters.AddWithValue("@contactPerson", txtConPerson.Text);
+                    cm.Parameters.AddWithValue("@telefono", txtPhone.Text);
+                    cm.Parameters.AddWithValue("@email", txtEmail.Text);
+                    cm.Parameters.AddWithValue("@fax", txtFaxNo.Text);
+                    cm.ExecuteNonQuery();
+                    cn.Close();
+                    MessageBox.Show("Proovedor guardado con exito!", "Guardado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Clear();
+                    supplier.LoadSupplier();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, stitle);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("Actualizar este Proveedor? click yes para confirmar.", "CONFIRMAR", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    cn.Open();
+                    cm = new SqlCommand("Update Proveedores set proveedor=@proveedor, direccion=@direccion, contactPerson=@contactPerson, telefono=@telefono, email=@email, fax=@fax where Id=@Id ", cn);
+                    cm.Parameters.AddWithValue("@Id", lblId.Text);
+                    cm.Parameters.AddWithValue("@proveedor", txtSupplier.Text);
+                    cm.Parameters.AddWithValue("@direccion", txtAddress.Text);
+                    cm.Parameters.AddWithValue("@contactPerson", txtConPerson.Text);
+                    cm.Parameters.AddWithValue("@telefono", txtPhone.Text);
+                    cm.Parameters.AddWithValue("@email", txtEmail.Text);
+                    cm.Parameters.AddWithValue("@fax", txtFaxNo.Text);
+                    cm.ExecuteNonQuery();
+                    cn.Close();
+                    MessageBox.Show("Proveedor actualizado correctamente!", "Actualizado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Warning");
+            }
+        }
+
+        private void SupplierModule_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Dispose();
+            }
+        }
+    }
+}
